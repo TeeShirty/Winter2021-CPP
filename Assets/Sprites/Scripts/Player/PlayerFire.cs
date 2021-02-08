31950e2590690312bdeb25dbd4f8178df15e8a0b
SlinkyCarp <sharathrshankar@outlook.com>
2021-02-08 12:38:33 -0500

    Revert "Revert "Added Projectile and Powerup""
    This reverts commit 4268fdff38c5ef6ac6daf5424bc5e67060df85a6.

---
+++ Assets/Sprites/Scripts/Player/PlayerFire.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class PlayerFire : MonoBehaviour
+{
+    SpriteRenderer marioSprite;
+
+    public Transform spawnPointLeft;
+    public Transform spawnPointRight;
+
+    public float projectileSpeed;
+    public Projectiles projectilesPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marioSprite = GetComponent<SpriteRenderer>();
+
+        if (projectileSpeed <= 0)
+        {
+            projectileSpeed = 7.0f;
+        }
+
+        if (!spawnPointLeft || !spawnPointRight || !projectilesPrefab)
+        {
+            Debug.Log("Unity inspector value not set");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireProjectile();
+        }
+    }
+
+    void FireProjectile()
+    {
+        if (marioSprite.flipX)
+        {
+            Debug.Log("Fire left side");
+            Projectiles projectileInstance = Instantiate(projectilesPrefab, spawnPointLeft.position, spawnPointLeft.rotation); //unity function 'instantiate()' will take a refernece to the object or to be created, will take vector 3 position and its rotation
+            projectileInstance.speed = projectileSpeed * -1;
+
+        }
+        else
+        {
+            Debug.Log("Fire Right side");
+            Projectiles projectileInstance = Instantiate(projectilesPrefab, spawnPointRight.position, spawnPointRight.rotation); //unity function 'instantiate()' will take a refernece to the object or to be created, will take vector 3 position and its rotation
+            projectileInstance.speed = projectileSpeed;
+        }
+    }
+}
---
+++ Assets/Sprites/Scripts/Projectiles/Projectiles.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Projectiles : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lifetime <= 0)
+        {
+            lifetime = 4.0f;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);  // setting velocity just one time when we start the object
+        Destroy(gameObject, lifetime); //lifetime is 2 seconds and will destroy itself after which
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Powerup")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
